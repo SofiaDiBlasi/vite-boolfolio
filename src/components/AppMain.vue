28d25bbf1acee849139472f7036ca81cc83a0e48
Sofia Di Blasi <phia.di.blasi@gmail.com>
2023-07-18 16:40:23 +0100

    paginazione

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -10,6 +10,8 @@
                 loading: false,
                 loadingError: false,
                 projects:[],
+                projectsCurrentPage: 0,
+                projectTotalPages: 0
             }
         }
     ,
@@ -17,13 +19,36 @@
         getProjects() {
             this.loading = true;
             axios.get(this.apiUrl + "projects").then((response) => {
-            this.projects = response.data.results;
-            console.log(this.projects)
+            this.projects = response.data.results.data;
+            this.projectsCurrentPage = response.data.results.current_page;
+            this.projectTotalPages = response.data.results.last_page;
             this.loading = false;
             }).catch(err => {
                 this.loading = false;
                 this.loadingError = err.message;
             });
+        },
+        nextPage() {
+            
+            if (this.projectsCurrentPage < this.projectTotalPages) {         ;
+
+                let config = {
+                    params: {
+                        page: (this.projectsCurrentPage+1)
+                    }
+                };
+                this.loading= true;
+                axios.get(this.apiUrl + "projects", config).then((response) => {
+                this.projects = response.data.results.data;
+                this.projectsCurrentPage = response.data.results.current_page;
+                console.log(this.projects)
+                this.loading = false;
+                }).catch(err => {
+                    this.loading = false;
+                    this.loadingError = err.message;
+                });
+
+            } 
         }
     },
     mounted() {
@@ -38,11 +63,21 @@
         <h4 v-if="loading">Caricamento in corso</h4>
         <h4 v-if="loadingError">{{ loadingError }}</h4>
 
+        <h2>Sei alla pagina {{ projectsCurrentPage }} di {{ projectTotalPages }}</h2>
+
         <div class="card" v-for="project in projects" v-bind:key="project">
             <h4>{{ project.name}}</h4>
+            <h5>Categoria: {{ project.type.name }}</h5>
+            <h5>Tecnologie usate:
+                <span v-for="technology in project.technologies" v-bind:key="technology"> 
+                    {{ technology.name }}
+                </span>
+            </h5>
             <p>{{ project.description}}</p>
             <p>{{ project.link}}</p>
         </div>
+
+        <a @click="nextPage" class="pageButton">Vai alla pagina successiva</a>
     </main>
 </template>
 
@@ -52,5 +87,15 @@
     padding: 1rem;
     margin: 1rem;
     border-radius: 0.25rem;
+}
+
+.pageButton {
+    padding: 0.5rem;
+    border-color: pink;
+    background: palegoldenrod;
+    color: white;
+    border-radius: 50px;
+    margin: 1rem;
+    cursor: pointer;
 }
 </style>
