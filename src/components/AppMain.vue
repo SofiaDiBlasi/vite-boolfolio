79a46a3e125081ab4f0419225bc0d9981d20fdeb
Sofia Di Blasi <phia.di.blasi@gmail.com>
2023-07-18 16:54:54 +0100

    impaginazione con nuovo componente

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,8 +1,12 @@
 <script>
     import axios from "axios";
+    import ProjectCard from "./ProjectCard.vue";
 
     export default{
         name :"AppMain",
+        components: {
+            ProjectCard
+         },
         data(){
             return {
                 chiave:"valore",
@@ -64,31 +68,17 @@
         <h4 v-if="loadingError">{{ loadingError }}</h4>
 
         <h2>Sei alla pagina {{ projectsCurrentPage }} di {{ projectTotalPages }}</h2>
-
-        <div class="card" v-for="project in projects" v-bind:key="project">
-            <h4>{{ project.name}}</h4>
-            <h5>Categoria: {{ project.type.name }}</h5>
-            <h5>Tecnologie usate:
-                <span v-for="technology in project.technologies" v-bind:key="technology"> 
-                    {{ technology.name }}
-                </span>
-            </h5>
-            <p>{{ project.description}}</p>
-            <p>{{ project.link}}</p>
-        </div>
-
+        <ProjectCard v-for="project in this.projects" :key="project" 
+            :name="project.name" 
+            :description="project.description" 
+            :technologies="project.technologies"
+            :type="project.type.name"
+            :link="project.link" />
         <a @click="nextPage" class="pageButton">Vai alla pagina successiva</a>
     </main>
 </template>
 
 <style scoped>
-.card {
-    background: pink;
-    padding: 1rem;
-    margin: 1rem;
-    border-radius: 0.25rem;
-}
-
 .pageButton {
     padding: 0.5rem;
     border-color: pink;
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,39 @@
+<script>
+    export default {
+        name: "ProjectCard",
+        props: {
+            name: String,
+            description: String,
+            type: String,
+            technologies: Array,
+            link : String
+        },
+        data(){
+            return{
+            }
+        }
+    }
+</script>
+
+<template>
+    <div class="card">
+        <h4>{{ name}}</h4>
+        <h5>Categoria: {{ type }}</h5>
+        <h5>Tecnologie usate:
+            <span v-for="technology in technologies" v-bind:key="technology"> 
+                {{ technology.name }}
+            </span>
+        </h5>
+        <p>{{ description}}</p>
+        <p>{{ link}}</p>
+    </div>
+</template>
+
+<style>
+.card {
+    background: pink;
+    padding: 1rem;
+    margin: 1rem;
+    border-radius: 0.25rem;
+}
+</style>
